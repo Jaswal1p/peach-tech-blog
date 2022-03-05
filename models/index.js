07b94ed4d9d4272839a6284912878e42ebfcf87d
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-05 16:16:20 -0500

    corrected models/index.js to make it less confusing

--- models/index.js
+++ models/index.js
@@ -4,15 +4,15 @@ const Article = require('./Article');
 const Comment = require('./Comment');
 
 User.hasMany(Article, {
-    foreignKey: 'author_id',
+    foreignKey: 'user_id',
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'commenter_id',
+    foreignKey: 'user_id',
 });
 
 Article.belongsTo(User, {
-    foreignKey: 'author_id'
+    foreignKey: 'user_id'
 });
 
 Article.hasMany(Comment, {
@@ -20,7 +20,7 @@ Article.hasMany(Comment, {
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'commenter_id',
+    foreignKey: 'user_id',
 });
 
 Comment.belongsTo(Article, {
