b62618adc17a68d97d47038ceb918c5844d4e0b4
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-05 16:54:42 -0500

    added express session npm plus connect withsequelize & updated server.js

--- server.js
+++ server.js
@@ -9,9 +9,31 @@ const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
+const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'super secret stuff',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+
+app.use(session(sess));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
