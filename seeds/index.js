2cdc3a55534c69629f894a92950237f602b81c7a
Jaswal1p <Jaswal1p@yahoo.com>
2022-03-06 23:51:55 -0500

    minor rearrangement of code

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const seedUsers = require('./user-seeds');
-const seedComments = require('./comment-seeds');
 const seedposts = require('./post-seeds');
+const seedComments = require('./comment-seeds');
+
 
 const sequelize = require('../config/connection');
 
@@ -10,13 +11,13 @@ const seedAll = async () => {
     
     await seedUsers();
       console.log('\n----- USERS SEEDED -----\n');
-      
-    await seedComments();
-      console.log('\n----- COMMENTS SEEDED -----\n');  
-
+        
     await seedposts();
       console.log('\n----- POSTS SEEDED -----\n');
       
+    await seedComments();
+      console.log('\n----- COMMENTS SEEDED -----\n');  
+  
     process.exit(0);  
 };
 
